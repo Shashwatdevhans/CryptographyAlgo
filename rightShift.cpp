710ac022978b6066ac36867e07ef72be715c4858
Shashwat Dev Hans <73686896+Shashwatdevhans@users.noreply.github.com>
2023-03-26 14:21:53 +0530

    Add files via upload

---
+++ rightShift.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+int rightShift(int n)
+{
+    int temp=n%10;
+    int rightShift=n/10;
+    int fact=1;
+    while(n/10)
+    {
+        fact*=10;
+        n/=10;
+    }
+    temp*=fact;
+    return temp+rightShift;
+}
+int main()
+{
+    int n=5;
+    int r=1234;
+    for(int i=0;i<n;i++)
+    {
+        r=rightShift(r);
+        cout<<r<<endl;
+    }
+    return 0;
+}
